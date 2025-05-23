55f828ca21a5a2527fd59784dc85b6c4e37a7cc8
Bashir HATOUM <bashir.hatoum@intalio.com>
2025-05-23 16:45:56 +0300

    book rating

--- src/routes/private/dashboard/+page.svelte
+++ src/routes/private/dashboard/+page.svelte
@@ -1,11 +1,48 @@
 <script lang="ts">
-	import { getUserState } from "$lib/state/user-state.svelte";
-
-    let userContext=getUserState();
-    let {allBooks}= $derived(userContext);
+	import { BookCard } from '$components';
+	import { getUserState } from '$lib/state/user-state.svelte';
+	import Icon from '@iconify/svelte';
+	let userContext = getUserState();
+	let { userName, allBooks } = $derived(userContext);
 </script>
-<ul>
-    {#each allBooks as book}
-        <li>{book.title}</li>
-    {/each}
-</ul>
+
+<div class="dashboard">
+	<div class="dashboard-header mb-m">
+		<a href="/private/scan-shelf" class="add-book">
+			<Icon icon="icons8:plus" width={'72'} height={'72'} />
+			<p>Add a book</p>
+		</a>
+		<div class="headline">
+			<h3 class="bold mb-xs">Welcome Back, {userName}</h3>
+			<p>
+				There's nothing quite like the journey a good book can take you on. Have you discovered any
+				new favorites recently?
+			</p>
+		</div>
+	</div>
+	{#each allBooks as book}
+		<BookCard {book} />
+	{/each}
+</div>
+
+<style>
+	.dashboard-header {
+		display: flex;
+		justify-content: space-between;
+		align-items: flex-start;
+		width: 100%;
+	}
+	.add-book {
+		display: flex;
+		align-items: center;
+		text-decoration: none;
+	}
+	.add-book p {
+		margin-left: 8px;
+	}
+	.headline {
+		text-align: right;
+		max-width: 30%;
+		min-width: 300px;
+	}
+</style>
